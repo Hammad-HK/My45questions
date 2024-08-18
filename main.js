5b568087fa1894f81c721ec8e03b899c246f539b
Hammad-HK <zhammad287@gmail.com>
2024-08-18 04:41:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//Question no 1
+console.log("hellow world");
+console.log("hello viewers");
+console.log("Pakistan zindabad");
+console.log("this is my channel");
+//Question no 2
+var myName = ('Hammad');
+var message = ('Hammad is a warrior');
+console.log(myName + " " + message);
---
+++ main.ts
@@ -0,0 +1,10 @@
+//Question no 1
+console.log("hellow world");
+console.log("hello viewers");
+console.log("Pakistan zindabad");
+console.log ("this is my channel");
+
+//Question no 2
+let myName=('Hammad');
+let message=('Hammad is a warrior');
+console.log(myName+" "+message);
